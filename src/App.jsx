122f073c26351c7b07c829360314f8d23a54dee6
Edwin Alexander Vargas <edwinalexandervargas07@gmail.com>
2026-05-18 14:34:23 -0700

    album list

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import Sidebar from './components/Sidebar'
-import Player from './components/Player'
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Player from "./components/Player";
+import Display from "./components/Display";
 
 const App = () => {
   return (
-    <div className='h-screen bg-black'>
-      <div className='h-[90%] flex'>
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
         <Sidebar />
+        <Display />
       </div>
-        <Player />
+      <Player />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
