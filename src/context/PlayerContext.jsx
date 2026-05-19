24f3735fe00c29391d7a1305494ccacf04c0b284
Edwin Alexander Vargas <edwinalexandervargas07@gmail.com>
2026-05-19 12:46:36 -0700

    added display to current and seek bar functionality

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useRef, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import { songsData } from "../assets/assets";
 
 export const PlayerContext = createContext();
@@ -23,13 +23,38 @@ const PlayerContextProvider = (props) => {
 
   const play = () => {
     audioRef.current.play();
-    setPlayStatus(true)
-  }
+    setPlayStatus(true);
+  };
 
   const pause = () => {
     audioRef.current.pause();
     setPlayStatus(false);
+  };
+
+  const playWithId = async (id) => {
+    await setTrack(songsData[id]);
+    await audioRef.current.play(); 
+    setPlayStatus(true);
   }
+
+  useEffect(() => {
+
+    setTimeout(() => {
+        audioRef.current.ontimeupdate = () => {
+        seekBar.current.style.width = (Math.floor(audioRef.current.currentTime / audioRef.current.duration * 100))+ "%"
+        setTime({
+          currentTime: {
+            second: Math.floor(audioRef.current.currentTime % 60),
+            minute: Math.floor(audioRef.current.currentTime / 60),
+          },
+          totalTime: {
+            second: Math.floor(audioRef.current.duration % 60),
+            minute: Math.floor(audioRef.current.duration / 60),
+          },
+        });
+      };
+    }, 1000);
+  }, [audioRef]);
 
   const contextValue = {
     audioRef,
@@ -41,7 +66,9 @@ const PlayerContextProvider = (props) => {
     setPlayStatus,
     time,
     setTime,
-    play, pause
+    play,
+    pause,
+    playWithId
   };
 
   return (
